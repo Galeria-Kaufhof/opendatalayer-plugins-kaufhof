3393544dfdccc1d4a299abb34a5b50f1c1b50746
Rico Pfaus <rico.pfaus@kaufhof.de>
2017-02-24 17:52:32 +0100

    fixed affilinet test

--- test/affilinet.js
+++ test/affilinet.js
@@ -1,71 +1,74 @@
 import { describe, it, beforeEach } from 'mocha';
-import { assert } from 'chai';
 import * as sinon from 'sinon';
 import System from 'systemjs';
-import './../../../../../systemjs.config';
-import mockModule from './../../../_mockModule';
-import * as dalDataTypes from './../../../../mocks/dalDataTypes';
+import mockModule from 'systemjs-mock-module';
+import * as odlDataTypes from 'opendatalayer-datatype-mocks';
+import jsdom from 'jsdom';
+import './../systemjs.config';
 
-describe('ba/lib/dal/bt/affilinet', () => {
-  let [Service, dalApi, dalDataMock, dalConfigMock, pixelHelperSpy, loggerSpy] = [];
+describe('opendatalayer-plugins-kaufhof/affilinet', () => {
+  let [Service, odlApi, odlDataMock, odlConfigMock, odlMock, loggerSpy] = [];
 
   beforeEach((done) => {
-    // mock data
-    dalApi = {};
-    dalDataMock = dalDataTypes.getDALGlobalDataStub('checkout-confirmation');
-    dalDataMock.order = dalDataTypes.getDALOrderDataStub([
-      dalDataTypes.getDALProductDataStub(123),
-      dalDataTypes.getDALProductDataStub(456),
-      dalDataTypes.getDALProductDataStub(789),
-    ]);
-    dalConfigMock = { site: 1234 };
     // spies
+    odlMock = {
+      window: jsdom.jsdom('<html><body></body></html>').defaultView,
+      Logger: () => loggerSpy,
+      helpers: { addScript: sinon.spy(), addImage: sinon.spy() },
+    };
     loggerSpy = { log: sinon.spy(), warn: sinon.spy() };
-    pixelHelperSpy = { addImage: sinon.spy() };
+    // mock data
+    odlApi = {};
+    odlDataMock = odlDataTypes.getODLGlobalDataStub('checkout-confirmation');
+    odlDataMock.order = odlDataTypes.getODLOrderDataStub([
+      odlDataTypes.getODLProductDataStub(123),
+      odlDataTypes.getODLProductDataStub(456),
+      odlDataTypes.getODLProductDataStub(789),
+    ]);
+    odlConfigMock = { site: 1234 };
     // register mocks
-    mockModule('gk/lib/logger', () => loggerSpy);
-    mockModule('ba/lib/pixelHelper', pixelHelperSpy);
+    mockModule(System, 'opendatalayer', odlMock);
     // clear module first
-    System.delete(System.normalizeSync('ba/lib/dal/bt/affilinet'));
-    System.import('ba/lib/dal/bt/affilinet').then(m => {
+    System.delete(System.normalizeSync('./src/plugins/affilinet'));
+    System.import('./src/plugins/affilinet').then((m) => {
       Service = m.default;
       done();
-    }).catch(err => { console.error(err); });
+    }).catch(err => console.error(err));
   });
 
   it("should add the pixel for pageType 'checkout-confirmation'", () => {
-    new Service(dalApi, dalDataMock, dalConfigMock);
-    sinon.assert.called(pixelHelperSpy.addImage);
+    new Service(odlApi, odlDataMock, odlConfigMock);
+    sinon.assert.called(odlMock.helpers.addImage);
   });
 
   it('should NOT add the pixel for any other pageType', () => {
-    dalDataMock.page.type = 'homepage';
-    new Service(dalApi, dalDataMock, dalConfigMock);
-    sinon.assert.notCalled(pixelHelperSpy.addImage);
+    odlDataMock.page.type = 'homepage';
+    new Service(odlApi, odlDataMock, odlConfigMock);
+    sinon.assert.notCalled(odlMock.helpers.addImage);
   });
 
   it('should add the correct URL and partner ID', () => {
-    new Service(dalApi, dalDataMock, dalConfigMock);
-    sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`//partners.webmasterplan.com/registersale.asp?site=${dalConfigMock.site}`));
+    new Service(odlApi, odlDataMock, odlConfigMock);
+    sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`//partners.webmasterplan.com/registersale.asp?site=${odlConfigMock.site}`));
   });
 
   it('should add the products info', () => {
-    new Service(dalApi, dalDataMock, dalConfigMock);
-    dalDataMock.order.products.map((p) => {
-      sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`ArticleNb%3D${p.ean}%26`));
-      sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`ProductName%3D${escape(p.name)}%26`));
-      sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`Category%3D${escape(encodeURIComponent(p.abteilungNummer))}%26`));
-      sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`Quantity%3D${escape(p.quantity)}%26`));
-      sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`SinglePrice%3D${escape(parseFloat(p.priceData.net).toFixed(2))}%26`));
-      sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match('Brand%3D%26'));
-      sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match('%0D%0A'));
+    new Service(odlApi, odlDataMock, odlConfigMock);
+    odlDataMock.order.products.map((p) => {
+      sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`ArticleNb%3D${p.ean}%26`));
+      sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`ProductName%3D${escape(p.name)}%26`));
+      sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`Category%3D${escape(encodeURIComponent(p.abteilungNummer))}%26`));
+      sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`Quantity%3D${escape(p.quantity)}%26`));
+      sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`SinglePrice%3D${escape(parseFloat(p.priceData.net).toFixed(2))}%26`));
+      sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match('Brand%3D%26'));
+      sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match('%0D%0A'));
     });
   });
 
   it('should add the order info', () => {
-    new Service(dalApi, dalDataMock, dalConfigMock);
-    sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`&price=${dalDataMock.order.priceData.net}`));
-    sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`&order=${dalDataMock.order.id}`));
-    sinon.assert.calledWith(pixelHelperSpy.addImage, sinon.match(`&vcode=${dalDataMock.order.couponCode}`));
+    new Service(odlApi, odlDataMock, odlConfigMock);
+    sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`&price=${odlDataMock.order.priceData.net}`));
+    sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`&order=${odlDataMock.order.id}`));
+    sinon.assert.calledWith(odlMock.helpers.addImage, sinon.match(`&vcode=${odlDataMock.order.couponCode}`));
   });
 });
