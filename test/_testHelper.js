102473f78527b4d86f84b2c43268ce726b6304e6
Rico Pfaus <rico.pfaus@kaufhof.de>
2017-02-24 18:48:36 +0100

    moved tests to spec path and added helper module to reduce boilerplate in spec tests

---
+++ test/_testHelper.js
@@ -0,0 +1,61 @@
+import * as sinon from 'sinon';
+import System from 'systemjs';
+import mockModule from 'systemjs-mock-module';
+import jsdom from 'jsdom';
+import './../systemjs.config';
+
+// plugin constructor
+let [Plugin] = [];
+
+/**
+ * Setup SystemJS module.
+ */
+export function setupModule(name) {
+  // clear module first
+  System.delete(System.normalizeSync(name));
+  return System.import(name).then((m) => {
+    Plugin = m.default;
+  }).catch(err => console.error(err));
+}
+
+/**
+ * Init and return object with mocks (ODL, logger, ...). Also registers
+ * ODL mock with SystemJS.
+ */
+export function initMocks() {
+  // spies
+  const loggerSpy = { log: sinon.spy(), warn: sinon.spy(), error: sinon.spy() };
+  const mocks = {
+    odl: {
+      window: jsdom.jsdom('<html><body></body></html>').defaultView,
+      Logger: () => loggerSpy,
+      helpers: { addScript: sinon.spy(), addImage: sinon.spy() },
+    },
+    logger: loggerSpy,
+  };
+  // register ODL mock within system
+  mockModule(System, 'opendatalayer', mocks.odl);
+
+  return mocks;
+}
+
+/**
+ * Returns the SystemJS instance used by this module.
+ */
+export function getSystemJSInstance() {
+  return System;
+}
+
+/**
+ * Returns the constructor for the associated ODL plugin.
+ */
+export function getPluginConstructor() {
+  return Plugin;
+}
+
+/**
+ * Returns the internal JSDOM instance.
+ */
+export function getJSDOM() {
+  return jsdom;
+}
