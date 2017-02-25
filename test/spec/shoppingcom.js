7519bfc3e43f3ba402f81962b3240e0f1cb07980
Rico Pfaus <rico.pfaus@kaufhof.de>
2017-02-25 10:42:55 +0100

    ported shopping.com plugin to ODL

--- test/spec/shoppingcom.js
+++ test/spec/shoppingcom.js
@@ -2,63 +2,56 @@
 import { describe, it, beforeEach } from 'mocha';
 import { assert } from 'chai';
 import * as sinon from 'sinon';
-import System from 'systemjs';
-import './../../../../../systemjs.config';
-import mockModule from './../../../_mockModule';
-import * as dalDataTypes from './../../../../mocks/dalDataTypes';
+import * as odlDataTypes from 'opendatalayer-datatype-mocks';
+import { setupModule, getPluginConstructor, initMocks, getJSDOM } from './../_testHelper';
 
-describe('ba/lib/dal/bt/shoppingcom', () => {
-  let [Service, dalApi, dalDataMock, dalConfigMock, windowSpy, loggerSpy, pixelHelperSpy, p1, p2, p3] = [];
+describe('shoppingcom', () => {
+  let [mocks, Plugin, odlApi, odlDataMock, odlConfigMock, p1, p2, p3] = [];
 
   beforeEach(() => {
-    windowSpy = { _roi: { push: sinon.spy() } };
-    dalApi = {};
-    dalDataMock = dalDataTypes.getDALGlobalDataStub('checkout-confirmation');
-    [p1, p2, p3] = [dalDataTypes.getDALProductDataStub(123), dalDataTypes.getDALProductDataStub(456), dalDataTypes.getDALProductDataStub(789)];
-    dalDataMock.order = dalDataTypes.getDALOrderDataStub([p1, p2, p3]);
-    dalConfigMock = { merchantId: '12345foo' };
-    // spies
-    loggerSpy = { log: sinon.spy(), warn: sinon.spy() };
-    pixelHelperSpy = { addScript: sinon.spy() };
-    // register mocks
-    mockModule('gk/globals/window', windowSpy);
-    mockModule('gk/lib/logger', () => loggerSpy);
-    mockModule('ba/lib/pixelHelper', pixelHelperSpy);
-    // clear module first
-    System.delete(System.normalizeSync('ba/lib/dal/bt/shoppingcom'));
-    return System.import('ba/lib/dal/bt/shoppingcom').then((m) => {
-      Service = m.default;
-    }).catch((err) => { console.error(err); });
+    odlApi = {};
+    odlDataMock = odlDataTypes.getODLGlobalDataStub('checkout-confirmation');
+    [p1, p2, p3] = [odlDataTypes.getODLProductDataStub(123), odlDataTypes.getODLProductDataStub(456), odlDataTypes.getODLProductDataStub(789)];
+    odlDataMock.order = odlDataTypes.getODLOrderDataStub([p1, p2, p3]);
+    odlConfigMock = { merchantId: '12345foo' };
+    // register mocks and overrides
+    mocks = initMocks();
+    mocks.odl.window._roi = { push: sinon.spy() };
+    // getJSDOM().changeURL(mocks.odl.window, 'https://example.com/foo');
+    // load module
+    return setupModule('./src/plugins/shoppingcom').then(() => {
+      Plugin = getPluginConstructor();
+    });
   });
 
   describe('checkout-confirmation', () => {
-    beforeEach(() => new Service(dalApi, dalDataMock, dalConfigMock));
+    beforeEach(() => new Plugin(odlApi, odlDataMock, odlConfigMock));
 
     it('should define a window._roi global', () => {
-      assert.isDefined(windowSpy._roi);
+      assert.isDefined(mocks.odl.window._roi);
     });
 
     it('should push a merchantId to _roi', () => {
-      sinon.assert.calledWith(windowSpy._roi.push, ['_setMerchantId', dalConfigMock.merchantId]);
+      sinon.assert.calledWith(mocks.odl.window._roi.push, ['_setMerchantId', odlConfigMock.merchantId]);
     });
 
     it('should push the total order revenue to _roi', () => {
-      sinon.assert.calledWith(windowSpy._roi.push, ['_setOrderAmount', dalDataMock.order.priceData.total]);
+      sinon.assert.calledWith(mocks.odl.window._roi.push, ['_setOrderAmount', odlDataMock.order.priceData.total]);
     });
 
     it('should push the products details to _roi', () => {
-      dalDataMock.order.products.forEach((p) => {
-        sinon.assert.calledWith(windowSpy._roi.push, ['_addItem',
+      odlDataMock.order.products.forEach((p) => {
+        sinon.assert.calledWith(mocks.odl.window._roi.push, ['_addItem',
           p.aonr, p.name, '', p.category, p.priceData.total, p.quantity]);
       });
     });
 
     it('should track a transaction event', () => {
-      sinon.assert.calledWith(windowSpy._roi.push, ['_trackTrans']);
+      sinon.assert.calledWith(mocks.odl.window._roi.push, ['_trackTrans']);
     });
 
     it('should append the shopping.com script to the DOM', () => {
-      sinon.assert.calledWith(pixelHelperSpy.addScript, sinon.match('//stat.dealtime.com/ROI/ROI2.js'));
+      sinon.assert.calledWith(mocks.odl.helpers.addScript, sinon.match('//stat.dealtime.com/ROI/ROI2.js'));
     });
   });
 });
